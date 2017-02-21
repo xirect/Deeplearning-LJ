3fa0956bd7fade96fc1a14d09028a151a94fc6b0
Jeffrey Hiraki <xirecthdesigns@gmail.com>
2017-02-21 20:44:28 +0100

    End of part 16
    Script to be finished in part 17, might not work as wanted yet, function not fully done.

--- Jeffrey/Week 2/Episode 15/euclideanDistance.py
+++ Jeffrey/Week 2/Episode 15/euclideanDistance.py
@@ -1,8 +1,28 @@
 from math import sqrt
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import style
+from collections import Counter
+import warnings
 
-plot1 = [1,3]
-plot2 = [2,5]
+style.use('fivethirtyeight')
+#2 classes 3 features each
+dataset =  {'k':[[1,2],[2,3],[3,1]], 'r':[[6,5],[7,7],[8,6]]}
+new_features = [5,7]
 
-euclidean_distance = sqrt( (plot1[0]-plot2[0])**2 + (plot1[1]-plot2[1])**2)
 
-print(euclidean_distance)
+# [[plt.scatter(ii[0], ii[1], s=100, color=i)for ii in dataset[i]] for i in dataset]
+# # De oneliner hierboven is hetzelfde als:
+# #for i in dataset:
+# #    for ii in dataset[i]:
+# #        plt.scatter(ii[0],ii[1], s=100, color=i)
+#
+# plt.scatter(new_features[0], new_features[1])
+# plt.show()
+
+def k_nearest_neighbors(data, predict, k=3):
+    if len(data) >= k:
+        warnings.warn('K is set to a value less than total voting groups!')
+
+        #knnalgos
+        #return vote_result
