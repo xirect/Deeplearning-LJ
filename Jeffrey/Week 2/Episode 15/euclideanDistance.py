1aec45abf8c41918d2a522b7a5df742499e15b6e
Jeffrey Hiraki <xirecthdesigns@gmail.com>
2017-02-21 21:31:12 +0100

    End of part 19, final code for euclideanDistance file

--- Jeffrey/Week 2/Episode 15/euclideanDistance.py
+++ Jeffrey/Week 2/Episode 15/euclideanDistance.py
@@ -5,11 +5,6 @@ import pandas as pd
 import random
 import warnings
 
-
-
-
-
-
 def k_nearest_neighbors(data, predict, k=3):
     if len(data) >= k:
         warnings.warn('K is set to a value less than total voting groups!')
@@ -21,41 +16,51 @@ def k_nearest_neighbors(data, predict, k=3):
             distances.append([euclidean_distance, group])
 
     votes = [i[1] for i in sorted(distances)[:k]]
-    #print(Counter(votes).most_common(1))
     vote_result = Counter(votes).most_common(1)[0][0]
+    confidence = Counter(votes).most_common(1)[0][1] / k
 
-    return vote_result
+    #print(confidence)
 
-df = pd.read_csv("breast-cancer-wisconsin.data.txt")
-df.replace('?',-99999, inplace=True)
-df.drop(['id'],1, inplace=True)
-full_data = df.astype(float).values.tolist()
-random.shuffle(full_data)
+    return vote_result, confidence
 
-test_size = 0.2
-train_set = {2:[],4:[]}
-test_set = {2:[],4:[]}
-train_data = full_data[:-int(test_size*len(full_data))]
-test_data = full_data[-int(test_size*len(full_data)):]
+accuracies = []
 
-for i in train_data:
-    train_set[i[-1]].append(i[:-1])
-
-for i in test_data:
-    test_set[i[-1]].append(i[:-1])
-
-correct = 0
-total = 0
-
-for group in test_set:
-    for data in test_set[group]:
-        vote = k_nearest_neighbors(train_set, data, k=5)
-        if group == vote:
-            correct +=1
-        total +=1
-print('Accuracy', correct/total)
+for i in range(5):
 
 
+    df = pd.read_csv("breast-cancer-wisconsin.data.txt")
+    df.replace('?',-99999, inplace=True)
+    df.drop(['id'],1, inplace=True)
+    full_data = df.astype(float).values.tolist()
+    random.shuffle(full_data)
+
+    test_size = 0.2
+    train_set = {2:[],4:[]}
+    test_set = {2:[],4:[]}
+    train_data = full_data[:-int(test_size*len(full_data))]
+    test_data = full_data[-int(test_size*len(full_data)):]
+
+    for i in train_data:
+        train_set[i[-1]].append(i[:-1])
+
+    for i in test_data:
+        test_set[i[-1]].append(i[:-1])
+
+    correct = 0
+    total = 0
+
+    for group in test_set:
+        for data in test_set[group]:
+            vote, confidence = k_nearest_neighbors(train_set, data, k=5)
+            if group == vote:
+                correct +=1
+            #else:
+            #    print(confidence)
+            total +=1
+    print('Accuracy', correct/total)
+    accuracies.append(correct/total)
+
+print(sum(accuracies)/len(accuracies))
 
 #Up to version 17
 #[[plt.scatter(ii[0], ii[1], s=100, color=i)for ii in dataset[i]] for i in dataset]
